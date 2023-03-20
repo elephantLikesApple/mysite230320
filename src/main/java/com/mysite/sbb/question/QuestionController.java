9cf03dfc2b4194d1b0d8fb1fbc1359638877cfc8
elephantLikesApple <elephant890@naver.com>
2023-03-20 15:15:23 +0900

    2-10 질문 상세

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,21 +1,31 @@
 package com.mysite.sbb.question;
 
+import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @AllArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        model.addAttribute("question", this.questionService.getQuestion(id));
+        return "question_detail";
     }
 }
